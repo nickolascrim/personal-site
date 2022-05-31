06c4563d3ea9ed7e142f3e4db58efea78c1c536a
nickolascrim <nickolas@hey.com>
2022-05-31 18:01:53 -0400

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  content: [
+  './pages/**/*.html',
+  './js/**/*.js',
+  './index.html'
+  ],
+  theme: {
+
+      fontFamily: {
+        sans: [ 
+        "Graphik", 
+        "Helvetica Neue", 
+        "helvetica", 
+        "Apple Color Emoji", 
+        "arial", 
+        "sans-serif"]
+    }
+
+
+  },
+  plugins: [],
+}
+
