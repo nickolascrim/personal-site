fd164c39a40506853443fe13c58cd87b0cf522e0
nickolascrim <nickolas@hey.com>
2022-06-01 08:43:14 -0400

    6/1 updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ module.exports = {
 
       fontFamily: {
         sans: [ 
-        "Graphik", 
+        "Basic sans", 
         "Helvetica Neue", 
         "helvetica", 
         "Apple Color Emoji", 
